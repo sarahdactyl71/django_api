b14e9f891f610c3242704a092631a44ff07b2da9
Sarah Kirk <kirkse710@gmail.com>
2018-08-14 11:53:17 -0600

    adds contact model

--- django_api/contact_api/models.py
+++ django_api/contact_api/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Contact(models.Model):
+    full_name = models.CharField(max_length=50)
+    email = models.CharField(max_length=100)
+    address = models.CharField(max_length=200)
+    phone = models.CharField(max_length=15)
