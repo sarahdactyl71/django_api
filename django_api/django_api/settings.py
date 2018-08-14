7ff05a6a7440656584b9fedc5ddf880622732fe1
Sarah Kirk <kirkse710@gmail.com>
2018-08-14 12:01:08 -0600

    updates how contact_api is called into app, makes migration file, migrates

--- django_api/django_api/settings.py
+++ django_api/django_api/settings.py
@@ -31,13 +31,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'contact_api.apps.ContactApiConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'contact_api',
 ]
 
 MIDDLEWARE = [
