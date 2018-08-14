5055700e6010e918fc3491556bddda2efa76f6a9
Sarah Kirk <kirkse710@gmail.com>
2018-08-14 13:58:00 -0600

    adds more logic to contact view

--- django_api/contact_api/urls.py
+++ django_api/contact_api/urls.py
@@ -3,5 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # ex: /contact_api/
     path('', views.index, name='index'),
+    # ex: /contact_api/5/
+    path('<int:contact_id>/', views.detail, name='detail'),
+    # ex: /contact_api/5/results/
+    path('<int:contact_id>/results/', views.results, name='results'),
 ]
--- django_api/contact_api/views.py
+++ django_api/contact_api/views.py
@@ -4,6 +4,18 @@
 
 from django.http import HttpResponse
 
+from django.shortcuts import render
+
+from .models import Contact
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the contact_api index.")
+    latest_contact_list = Contact.objects.order_by('-email')[:5]
+    context = {'latest_contact_list': latest_contact_list}
+    return render(request, 'contact_api/index.html', context)
+
+def detail(request, contact_id):
+    return HttpResponse("You're looking at contact %s." % contact_id)
+
+def results(request, contact_id):
+    response = "You're looking at the results of contact %s."
+    return HttpResponse(response % contact_id)
